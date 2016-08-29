984b97ba5bc5a728116d7b12b6b7916454f02639
abryrath <arath125@gmail.com>
2016-08-29 15:16:24 -0400

    added bower/npm

---
+++ src/main/java/com/theironyard/charlotte/RestaurantAngularApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.charlotte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantAngularApplication.class, args);
+	}
+}
