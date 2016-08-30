3dba3447c8af88d7c31db798ac67a29b455bb580
abryrath <arath125@gmail.com>
2016-08-30 14:45:34 -0400

    angular totally working :D

--- public/app/controller.js
+++ public/app/controller.js
@@ -2,18 +2,18 @@
 
 var app = angular.module('restaurants.controllers', []);
 
-app.controller('RestaurantListController', ['$scope', 'RestaurantsFactory', 'RestaurantFactory', '$location', 
-    function($scope, RestaurantsFactory, RestaurantFactory, $location) {
+app.controller('RestaurantListController', ['$scope', 'RestaurantFactory', '$location', 
+    function($scope, RestaurantFactory, $location) {
         //callback for editRestaurant() [ng-click]
         $scope.editRestaurant = function(restaurantId) {
-            console.log("editRestaurant("+restaurantId+")");
             $location.path('/restaurant-detail/' + restaurantId);
         };
 
         //callback for deleteRestaurant() [ng-click]
         $scope.deleteRestaurant = function(restaurantId) {
-            RestaurantFactory.delete({ id: restaurantId });
-            $scope.restaurants = RestaurantsFactory.query();
+            RestaurantFactory.delete({ id: restaurantId }).then(function() {
+                $scope.restaurants = RestaurantFactory.getAll();
+            });
         };
 
         //callback for createRestaurant() [ng-click]
@@ -21,37 +21,37 @@ app.controller('RestaurantListController', ['$scope', 'RestaurantsFactory', 'Res
             $location.path('/restaurant-create');
         };
 
-        $scope.restaurants = RestaurantsFactory.query();
-}]);
+        $scope.restaurants = RestaurantFactory.getAll();
+    }
+]);
 
 
 app.controller('RestaurantDetailController', ['$scope', '$routeParams', 'RestaurantFactory', '$location',
     function($scope, $routeParams, RestaurantFactory, $location) {
-        console.log('RestaurantDetailController');
         //callback for updateRestaurant [ng-click]
         $scope.updateRestaurant = function() {
-            RestaurantFactory.update($scope.restaurant);
-            $location.path('/restaurants');
+            RestaurantFactory.update($scope.restaurant).then(function() {
+                $location.path('/restaurants');
+            });
         }
 
         //callback for cancel [ng-click]
         $scope.cancel = function() {
             $location.path('/restaurants');
         }
-        
-        console.log("$routeParams.id = "+$routeParams.id);
-        $scope.restaurant = RestaurantFactory.show({ id: $routeParams.id });
-
-}]);
+        $scope.restaurant = RestaurantFactory.get($routeParams.id);//{id: $routeParams.id});
+    }
+]);
 
 
-app.controller('RestaurantCreationController', ['$scope', 'RestaurantsFactory', '$location', 
-    function($scope, RestaurantsFactory, $location) {
+app.controller('RestaurantCreationController', ['$scope', 'RestaurantFactory', '$location', 
+    function($scope, RestaurantFactory, $location) {
 
         //callback for createNewRestaurant() [ng-click]
         $scope.createNewRestaurant = function() {
-            RestaurantsFactory.create($scope.restaurant);
-            $location.path('/restaurants');
+            RestaurantFactory.create($scope.restaurant).then(function() {      
+                $location.path('/restaurants');
+            });
         }
 
 }]);
--- public/app/services.js
+++ public/app/services.js
@@ -2,34 +2,82 @@
 
 var services = angular.module('restaurants.services', ['ngResource']);
 
-services.factory('RestaurantsFactory', function($resource){
-    return $resource('/api/restaurants', {}, {
-        query: {
-            method: 'GET',
-            isArray: true
+services.factory('RestaurantFactory', function ($http) {
+    return {
+        create: function (restaurant) {
+            return $http({
+                method: 'POST',
+                url: '/api/restaurants',
+                data: restaurant,
+            });
         },
-        create: {
-            method: 'POST'
+        get: function(id){
+            let data = {};
+            $http({
+                method: 'GET',
+                url: '/api/restaurant/'+id
+            }).then(function(info) {
+                angular.copy(info.data, data);
+            })
+            return data;
+        },
+        getAll: function () {
+            let spots = [];
+
+            $http({
+                method: 'GET',
+                url: '/api/restaurants',
+            }).then(function (info) {
+                // Special function that copies an array and then notifies angular that it needs to re-render
+                angular.copy(info.data, spots);
+            });
+
+            return spots;
+        },
+        update: function(restaurant){
+            return $http({
+                method: 'PUT',
+                url: '/api/restaurant/'+restaurant.id,
+                data: JSON.stringify(restaurant),
+            })
+        },
+        delete: function(restaurant){
+            return $http({
+                method: 'DELETE',
+                url: '/api/restaurant/'+restaurant.id,
+            })
         }
-    })
+    };
 });
 
-services.factory('RestaurantFactory', function($resource){
-    return $resource('/api/restaurant/:id', {}, {
-        show: {
-            method: 'GET'
-        },
-        update: {
-            method: 'PUT',
-            params: {
-                id: '@id'
-            }
-        },
-        delete: {
-            method: 'DELETE',
-            params: {
-                id: '@id'
-            }
-        }
-    })
-});
+// services.factory('RestaurantsFactory', function($http){
+//     // return $resource('/api/restaurants', {}, {
+//     //     query: {
+//     //         method: 'GET',
+//     //         isArray: true
+//     //     },
+//     //     create: {
+//     //         method: 'POST'
+//     //     }
+//     // })
+// });
+
+// services.factory('RestaurantFactory', function($http){
+//     // return $resource('/api/restaurant/:id', {}, {
+//     //     show: {
+//     //         method: 'GET'
+//     //     },
+//     //     update: {
+//     //         method: 'PUT',
+//     //         params: {
+//     //             id: '@id'
+//     //         }
+//     //     },
+//     //     delete: {
+//     //         method: 'DELETE',
+//     //         params: {
+//     //             id: '@id'
+//     //         }
+//     //     }
+//     // })
+// });
